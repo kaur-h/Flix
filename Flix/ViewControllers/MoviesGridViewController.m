2c8c5a52dffc056fd9255a7f68d44bc4b30502b5
kaur-h <harleenk120@gmail.com>
2021-06-24 17:30:02 -0400

    Add Collection View

---
+++ Flix/ViewControllers/MoviesGridViewController.m
@@ -0,0 +1,138 @@
+//
+//  MoviesGridViewController.m
+//  Flix
+//
+//  Created by Harleen Kaur on 6/24/21.
+//
+
+#import "MoviesGridViewController.h"
+#import "MovieCollectionCell.h"
+#import "UIImageView+AFNetworking.h"
+#import "DetailsViewController.h"
+
+@interface MoviesGridViewController () <UICollectionViewDelegate, UICollectionViewDataSource>
+
+@property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
+@property (nonatomic, strong) NSArray *movies;
+@property (nonatomic,strong) UIRefreshControl *refreshControl;
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *activityIndicator;
+
+@end
+
+@implementation MoviesGridViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.collectionView.dataSource = self;
+    self.collectionView.delegate = self;
+    
+    // Start the activity indicator
+    [self.activityIndicator startAnimating];
+    [self.view bringSubviewToFront:self.activityIndicator];
+    
+    [self fetchMovies];
+    
+    //Refresh Control
+    self.refreshControl = [[UIRefreshControl alloc] init];
+    self.refreshControl.tintColor = UIColor.whiteColor;
+    [self.refreshControl addTarget:self action:@selector(fetchMovies) forControlEvents:UIControlEventValueChanged];
+    [self.collectionView insertSubview:self.refreshControl atIndex:0];
+    
+    //Layout
+    UICollectionViewFlowLayout *layout = (UICollectionViewFlowLayout *) self.collectionView.collectionViewLayout;
+    
+    layout.minimumLineSpacing = 5;
+    layout.minimumInteritemSpacing = 5;
+    
+    CGFloat postersPerLine = 2;
+    CGFloat itemWidth = (self.collectionView.frame.size.width - layout.sectionInset.left - layout.sectionInset.right - layout.minimumInteritemSpacing * (postersPerLine - 1)) / postersPerLine;
+    layout.itemSize = CGSizeMake( itemWidth, (itemWidth - layout.sectionInset.top)*1.5);
+    
+    
+}
+
+- (void) fetchMovies {
+    // Getting the API
+    NSURL *url = [NSURL URLWithString:@"https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
+    NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+           if (error != nil) {
+               NSLog(@"%@", [error localizedDescription]);
+               //Creating the Alert
+               UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Cannot Get Movies"
+                                                                                          message:@"The internet connection appears to be offline"
+                                                                                   preferredStyle:(UIAlertControllerStyleAlert)];
+               
+               // create an try again action
+               UIAlertAction *tryAgainAction = [UIAlertAction actionWithTitle:@"Try Again"
+                                                                  style:UIAlertActionStyleDefault
+                                                                handler:^(UIAlertAction * _Nonnull action) {
+                                                                        // handle response here.
+                                                                        // Start the activity indicator
+                                                                        [self.activityIndicator startAnimating];
+                                                                        [self.view bringSubviewToFront:self.activityIndicator];
+                   
+                                                                        [self fetchMovies];
+                                                                }];
+               // add the try again action to the alert controller
+               [alert addAction:tryAgainAction];
+               
+               [self presentViewController:alert animated:YES completion:^{
+                   // optional code for what happens after the alert controller has finished presenting
+               }];
+           }
+           else {
+                   NSDictionary *dataDictionary = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
+
+                   // TODO: Get the array of movies
+                   self.movies = dataDictionary[@"results"];
+                   // TODO: Store the movies in a property to use elsewhere
+                   // TODO: Reload your table view data
+                   [self.collectionView reloadData];
+               
+           }
+        [self.activityIndicator stopAnimating];
+        [self.refreshControl endRefreshing];
+       }];
+    
+    [task resume];
+}
+
+
+- (nonnull __kindof UICollectionViewCell *)collectionView:(nonnull UICollectionView *)collectionView cellForItemAtIndexPath:(nonnull NSIndexPath *)indexPath {
+    MovieCollectionCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"MovieCollectionCell" forIndexPath:indexPath];
+    
+    NSDictionary *movie = self.movies[indexPath.item];
+    
+    NSString *baseURLString = @"https://image.tmdb.org/t/p/w500";
+    NSString *posterURL = movie[@"poster_path"];
+    NSString *fullPosterURLString = [baseURLString stringByAppendingString: posterURL];
+    
+    NSURL *fullPosterURL = [NSURL URLWithString:fullPosterURLString];
+    cell.posterImage.image = nil;
+    [cell.posterImage setImageWithURL:fullPosterURL];
+    return cell;
+}
+
+- (NSInteger)collectionView:(nonnull UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    return  self.movies.count;
+}
+
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+ UICollectionViewCell *tappedCell = sender;
+    NSIndexPath *indexPath = [self.collectionView indexPathForCell:tappedCell];
+ NSDictionary *movie = self.movies[indexPath.row];
+ DetailsViewController *detailController = [segue destinationViewController];
+ 
+ detailController.movie = movie;
+}
+
+
+@end
